d2dd270931a053110f2742ce6b5ca4d2bc30be42
FannyCESI <fanny.alfonso@viacesi.fr>
2024-05-14 14:55:57 +0200

    API COntroller

--- exo.Controller/Program.cs
+++ exo.Controller/Program.cs
@@ -1,38 +1,56 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using exo.Business;
 using exo.Common.Models;
+
+
 namespace exo.Controllers;
 
-public class HomeController : ControllerBase
+[ApiController] // On définit que c'est un controleur
+[Route("exo.controllers")] // A l'intérieur des guillemets on met l'url qu'on donnera à l'externe pour connexion à l'API    
+
+public class HomeController : ControllerBase 
 {
     public HomeController(){
-
     } 
 
-    [ApiController] // On définit que c'est un controleur
-    [Route("exo.controllers")] // A l'intérieur des guillemets on met l'url qu'on donnera à l'externe pour connexion à l'API    
+    private readonly IArticlesServices _articleServices ; 
 
-    [HttpPost]
-    public IActionResult AddArticle(Article article){
-        var result = 
-        return (result);
-    } 
-
-    [HttpPost ("update")]
-    public IActionResult UpdateArticle(int Id){
-        return Ok (result);
+    //Initialisation de la dépendance 
+    public HomeController(IArticlesServices articlesServices)
+    {
+        _articleServices = articlesServices;
     }
 
-    [HttpGet ("{id}")] 
-    public IActionResult Display1Article(int id){
-        return (result);
+
+    // Appel des fonctions de CRUD de exo.Services
+    [HttpPost]
+    public IActionResult AddArticle(Article article)
+    {
+        var result = _articleServices.Add(article);
+        return Ok(result); 
+    } 
+
+    [HttpPost("update")]
+    public IActionResult UpdateArticle(int Id)
+    {
+        var result = _articleServices.Update(Id); 
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")] 
+    public IActionResult Display1Article(int id)
+    {
+        var result = _articleServices.Get(id); 
+        return Ok(result);
     }
 
     [HttpDelete]
-    public IActionResult DeleteArticle(int id){
-        return(result);
+    public IActionResult DeleteArticle(int id)
+    {
+        var result = _articleServices.Delete(id); 
+        return Ok(result);
     }
+
 }
 
     
